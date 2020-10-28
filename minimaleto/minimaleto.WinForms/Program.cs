41841b0a37a9acf52f08880e8cad37c229a8fab9
seirra <sophietheopossum@yandex.com>
2020-10-28 11:43:40 +0000

    add winforms compatibility

---
+++ minimaleto/minimaleto.WinForms/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Eto.Forms;
+
+namespace minimaleto.WinForms
+{
+    internal static class MainClass
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            new Application(Eto.Platforms.WinForms).Run(new MainForm());
+        }
+    }
+}
